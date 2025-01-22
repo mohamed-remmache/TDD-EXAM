3ad5a312801855e0467000e9e192ac989fabb68b
mohamed-remmache <remachenourallah@gmail.com>
2025-01-22 16:24:04 +0100

    first Test - no implementation

--- test_main.py
+++ test_main.py
@@ -1,2 +1,12 @@
 import unittest
 from main import move_rover
+class TestMoveRover(unittest.TestCase):
+    def test_basic_case(self):
+        grid = (5, 5)
+        position = (1, 2, 'N')
+        commands = "LMLMLMLMM"
+        expected = (1, 3, 'N')
+        self.assertEqual(move_rover(grid, position, commands), expected)
+
+if __name__ == "__main__":
+    unittest.main()
